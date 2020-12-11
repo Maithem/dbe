720b843854f2adba76de34270574aa1ce75c3f06
Maithem <Maithem@users.noreply.github.com>
2020-12-11 01:40:28 -0800

    README.md Description
    Added sample DB load logic + Logger
    
    First Test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
- 
+# dbe 
+
+The purpose of this repo is to provide tools to easily gather per-process block level IO statistics.
+This is useful when evaluating the read, write and space amplification for different storage engines. Since many storage
+engines rely on "non-explicit" IO (i.e., mmap), mining /proc/[PID]/io is not sufficient. Alternatively, this tool
+creates a filesystem backed by a loopback device. The storage engine can then exclusively use the "virtual filesystem",
+and block level stats can mined from https://www.kernel.org/doc/Documentation/block/stat.txt
--- main.c
+++ main.c
@@ -3,31 +3,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <stdint.h>
 
-void lmdb_failure(int rc, char *msg) {
-  fprintf(stderr, "%s: %s\n", msg, mdb_strerror(rc));
-  exit(1);
-}
+#include "macrologger.h"
+
+#define LMDB_FAILURE(rc) \
+            do {         \
+            LOG_ERROR("%s", mdb_strerror(rc)); \
+            exit(1);            \
+            } while (0)
+
 int main() {
 
-  uint num_keys = 1000;
+  LOG_INFO("Started!");
+
+  uint num_keys = 10000d000;
   size_t key_size = 100;
   size_t val_size = 100;
 
   char *db_path = "/tmp/lmdb";
+  char *db_data_path = "/tmp/lmdb/data.mdb";
 
   int rc;
   MDB_env *env;
   MDB_txn *txn;
   MDB_dbi dbi;
-  int env_opt = 0;
-  char file_name[100];
+  int env_opt = 0 | MDB_NOSYNC;
   size_t msize;
   unsigned int max_readers = 8;
 
   rc = mkdir(db_path, 0775);
   if (rc && errno != EEXIST) {
     perror("mkdir");
+    exit(1);
+  }
+
+  rc = remove(db_data_path);
+  if (rc) {
+    perror("remove");
     exit(1);
   }
 
@@ -38,34 +51,69 @@ int main() {
 
   rc = mdb_env_create(&env);
   if (rc)
-    lmdb_failure(rc, "create environment error");
+    LMDB_FAILURE(rc);
 
-  msize = num_keys * 32L * val_size / 10;
+  msize = num_keys * 32L * (val_size + key_size) / 10;
   rc = mdb_env_set_mapsize(env, msize);
   if (rc)
-    lmdb_failure(rc, "set size error");
+    LMDB_FAILURE(rc);
 
   rc = mdb_env_set_maxreaders(env, max_readers + 2);
   if (rc)
-    lmdb_failure(rc, "set max readers");
+    LMDB_FAILURE(rc);
 
   rc = mdb_env_open(env, db_path, env_opt, 0664);
   if (rc)
-    lmdb_failure(rc, "open error");
+    LMDB_FAILURE(rc);
 
   rc = mdb_txn_begin(env, NULL, 0, &txn);
   if (rc)
-    lmdb_failure(rc, "txn begin error");
+    LMDB_FAILURE(rc);
 
   // Passing null to open will only create a single table in the environment
   rc = mdb_open(txn, NULL, 0, &dbi);
   if (rc)
-    lmdb_failure(rc, "mdb open error");
+    LMDB_FAILURE(rc);
 
   rc = mdb_txn_commit(txn);
   if (rc)
-    lmdb_failure(rc, "txn commit error");
+    LMDB_FAILURE(rc);
 
-  printf("Hello, World!\n");
+  MDB_val key;
+  MDB_val data;
+
+  char key_arr[key_size];
+  char val_arr[val_size];
+  key.mv_data = key_arr;
+  key.mv_size = sizeof(key_arr);
+  data.mv_data = val_arr;
+  data.mv_size = sizeof(val_arr);
+
+  for (uint64_t idx = 0; idx < num_keys; idx++) {
+
+    uint64_t *ptr = key_arr;
+    ptr[0] = idx;
+
+    rc = mdb_txn_begin(env, NULL, 0, &txn);
+    if (rc)
+      LMDB_FAILURE(rc);
+
+    rc = mdb_put(txn, dbi, &key, &data, 0);
+    if (rc)
+      LMDB_FAILURE(rc);
+
+    rc = mdb_txn_commit(txn);
+    if (rc)
+      LMDB_FAILURE(rc);
+  }
+
+  LOG_INFO("Syncing Data!");
+
+  rc = mdb_env_sync(env, 1);
+  if (rc)
+    LMDB_FAILURE(rc);
+
+  LOG_INFO("Finished!");
+
   return 0;
 }
